03eab75bb4fa33c1e0f00ad8d94baaad49b3974f
nagarajan1986 <nagu.tcl@gmail.com>
2022-07-17 18:11:15 +0530

    new added

---
+++ src/test/java/org/test/Testing.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class Testing {
+	
+	
+	private void method1() {
+		
+		System.out.println("new code added");
+
+	}
+	
+private void method2() {
+		
+		System.out.println("new code added");
+
+	}
+
+}
